c57c6b387d948290f684490cf956b0f68fae5961
AlamoFiteCat <shredgod_lobos@hotmail.com>
2021-11-03 23:05:22 +0100

    Added more mern stuff

--- mern-app/src/components/ExpenseItem/ExpenseItem.js
+++ mern-app/src/components/ExpenseItem/ExpenseItem.js
@@ -23,21 +23,23 @@ const ExpenseItem = (props) => {
 
   const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    console.log("Say Hello");
-    // title="Updated!";
-    setTitle("Updated");
-  };
+  // const clickHandler = () => {
+  //   console.log("Say Hello");
+  //   // title="Updated!";
+  //   setTitle("Updated");
+  // };
 
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">{props.amount}</div>
+        </div>
+        {/* <button onClick={clickHandler}>Change Title</button> */}
+      </Card>
+    </li>
   );
 };
 
--- mern-app/src/components/Expenses/Expenses.js
+++ mern-app/src/components/Expenses/Expenses.js
@@ -1,7 +1,53 @@
+// import React, { useState } from "react";
+// import Card from "../Card/Card";
+// import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
+// import ExpenseItem from "../ExpenseItem/ExpenseItem";
+// import "./Expenses.css";
+
+// const Expenses = (props) => {
+//   const [filteredYear, setFilteredYear] = useState("2020");
+//   const filterChangeHandler = (selectedYear) => {
+//     setFilteredYear(selectedYear);
+//   };
+
+//   return (
+//     <div>
+//       <Card className="expenses">
+//         <ExpensesFilter
+//           onChangeFilter={filterChangeHandler}
+//           selected={filteredYear}
+//         />
+//         <ExpenseItem
+//           title={props.expenses[0].title}
+//           amount={props.expenses[0].amount}
+//           date={props.expenses[0].date}
+//         />
+//         <ExpenseItem
+//           title={props.expenses[1].title}
+//           amount={props.expenses[1].amount}
+//           date={props.expenses[1].date}
+//         />
+//         <ExpenseItem
+//           title={props.expenses[2].title}
+//           amount={props.expenses[2].amount}
+//           date={props.expenses[2].date}
+//         />
+//         <ExpenseItem
+//           title={props.expenses[3].title}
+//           amount={props.expenses[3].amount}
+//           date={props.expenses[3].date}
+//         />
+//       </Card>
+//     </div>
+//   );
+// };
+
+// export default Expenses;
+
 import React, { useState } from "react";
 import Card from "../Card/Card";
 import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
-import ExpenseItem from "../ExpenseItem/ExpenseItem";
+import ExpensesList from "../ExpensesList/ExpensesList";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -10,6 +56,9 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter(
+    (item) => item.date.getFullYear().toString() === filteredYear
+  );
   return (
     <div>
       <Card className="expenses">
@@ -17,26 +66,7 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
           selected={filteredYear}
         />
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        />
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        />
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        />
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
